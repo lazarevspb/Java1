7476bd05af605b557803b581b17b09a3ecfaea99
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-08-08 11:01:54 +0300

    lesson 4 upd + refactor

--- src/lesson4/Employee.java
+++ src/lesson4/Employee.java
@@ -1,17 +1,19 @@
 package lesson4;
 
-
 /* √ 1. Создать класс "Сотрудник" с полями: ФИО, зарплата, возраст;*/
+import java.util.Formatter;
+
 /**
  * Класс "Person" - содержит информацию о сотруднниках.
  */
-public class Person {
-
-    private static int personID;
+public class Employee {
+    private static final int CURRENT_YEAR = 2020;
+    private static int employeeID;
     private String name;
     private float salary;
-    private int age;
-    private int id;
+    private int birthYear;
+    private int uid;
+    Formatter f = new Formatter();
 
     /* √ 3. Внутри класса «Сотрудник» написать методы, которые возвращают значение каждого поля;*/
     public String getName() {
@@ -23,29 +25,35 @@ public class Person {
     }
 
     public int getAge() {
-        return age;
+        return CURRENT_YEAR - birthYear;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    Formatter getFullInfo(){
+        f.format("ID: %d, Сотрудник: %s, Зарплата: %,.2f руб., Возраст: %d лет,",
+                this.getUid(), this.getName(), this.getSalary(), this.getAge());
+        return  f;
     }
 
     public void setSalary(float salary) {
         this.salary = salary;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    private void setId(int id) {
-        this.id = id;
+    private void setUid(int uid) {
+        this.uid = uid;
     }
 
     /* √ 2. Конструктор класса должен заполнять эти поля при создании объекта;*/
-    public Person(String name, float salary, int age) {
+    public Employee(String name, float salary, int birthYear) {
          /* √ 8. *** Продумать конструктор таким образом, чтобы при создании каждому сотруднику
          присваивался личный уникальный идентификационный порядковый номер*/
-        personID++;
+        employeeID++;
         this.name = name;
         this.salary = salary;
-        this.age = age;
-        setId(personID);
+        this.birthYear = birthYear;
+        setUid(employeeID);
     }
 }
--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -4,74 +4,71 @@ package lesson4;
  * Homework for lesson #4
  *
  * @author Valeriy Lazarev
- * @since 2020-08-01
+ * @since 2020-08-08
  */
-
 
 public class Main {
 
-
     /* √ 5. Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет;*/
-    public static Person[] arrPerson = {new Person("Пал Палыч", 25000.00f, 63),
-            new Person("Володарыч", 20000.00f, 50),
-            new Person("Сан Саныч", 20000.00f, 60),
-            new Person("Алисей Алисеич", 30000.00f, 64),
-            new Person("Петр", 55000.00f, 18),
+    public static Employee[] arrEmployees = {
+            new Employee("Пал Палыч", 25000.00f, 1957),
+            new Employee("Володарыч", 20000.00f, 1970),
+            new Employee("Сан Саныч", 20000.00f, 1960),
+            new Employee("Алисей Алисеич", 30000.00f, 1956),
+            new Employee("Петр", 55000.00f, 2002),
     };
 
     public static void main(String[] args) {
-
         /* √ 6. * Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000.*/
         float rise = 5000.00f;
-        salaryUp(rise);
+        salaryUp(rise, 45);
 
         /* √ 4. Вывести при помощи методов из пункта 3 ФИО и возраст.*/
         printPerson();
 
         /* √ 7. * Подсчитать средние арифметические зарплаты и возраста*/
-        System.out.printf("Средняя зарплата по больнице: %,.2f руб.\n", averageSalary(arrPerson));
-        System.out.println("Средний возраст по больнице: " + averageAge(arrPerson));
-
-
+        System.out.printf("Средняя зарплата по больнице: %,.2f руб.\n", averageSalary(arrEmployees));
+        System.out.println("Средний возраст по больнице: " + averageAge(arrEmployees) + ". ");
     }
 
     /**
-     *  * Метод считает среднюю арифмитическую зарплату;
+     * * Метод считает среднюю арифмитическую зарплату;
      *
-     * @param arrPerson - Массив, для которого считается среднее арифметическое значение;
+     * @param arrEmployees - Массив, для которого считается среднее арифметическое значение;
      * @return -  Возвращает целочисленную переменную равную средней зарплате всех сотрудников содержашихся в массиве
      */
-    private static int averageAge(Person[] arrPerson) {
+    private static int averageAge(Employee[] arrEmployees) {
         int result = 0;
-        for (Person person : arrPerson) {
-            result += person.getAge();
+        for (Employee employee : arrEmployees) {
+            result += employee.getAge();
         }
-        return  result/arrPerson.length;
+        return result / arrEmployees.length;
     }
 
     /**
-     *  * Метод считает среднюю арифмитический возраст;
+     * * Метод считает среднюю арифмитический возраст;
      *
-     * @param arrPerson - Массив, для которого считается среднее арифметическое значение;
+     * @param arrEmployees - Массив, для которого считается среднее арифметическое значение;
      * @return -  Возвращает целочисленную переменную равную среднему возрасту всех сотрудников содержашихся в массиве
      */
-    private static float averageSalary(Person[] arrPerson) {
+    private static float averageSalary(Employee[] arrEmployees) {
         float result = 0;
-        for (Person person : arrPerson) {
-            result += person.getSalary();
+        for (Employee employee : arrEmployees) {
+            result += employee.getSalary();
         }
-        return  result/arrPerson.length;
-
+        return result / arrEmployees.length;
     }
 
     /**
-     * Метод, который повышает зарплату всем сотрудникам старше 45 лет на переданное значение
-     * @param rise - переменная с плавающей точкой на значение которой метод увеличивает заработную плату работника.
+     *   * Метод, который повышает зарплату всем сотрудникам старше 45 лет на переданное значение
+     *
+     *  @param rise - переменная с плавающей точкой на значение которой метод увеличивает заработную плату работника.
+     * @param age - целочисленная переменаая - возраст сотрудника.
      */
-    private static void salaryUp(float rise) {
-        for (Person person : arrPerson) {
-            if (person.getAge() >= 45) {
-                person.setSalary(person.getSalary() + rise);
+    private static void salaryUp(float rise, int age) {
+        for (Employee employee : arrEmployees) {
+            if (employee.getAge() >= age) {
+                employee.setSalary(employee.getSalary() + rise);
             }
         }
     }
@@ -80,11 +77,21 @@ public class Main {
      * Метод выводит в консоль данные о сотрудниках.
      */
     private static void printPerson() {
-        for (Person person : arrPerson) {
-            System.out.printf("ID: %d, Сотрудник: %s, Зарплата: %,.2f руб., Возраст: %d лет, \n",
-                    person.getId(), person.getName(), person.getSalary(), person.getAge());
+        for (Employee employee : arrEmployees) {
+            System.out.println(employee.getFullInfo());
         }
     }
 
 
 }
+/*
+
+Вывод:
+        ID: 1, Сотрудник: Пал Палыч, Зарплата: 30 000,00 руб., Возраст: 63 лет,
+        ID: 2, Сотрудник: Володарыч, Зарплата: 25 000,00 руб., Возраст: 50 лет,
+        ID: 3, Сотрудник: Сан Саныч, Зарплата: 25 000,00 руб., Возраст: 60 лет,
+        ID: 4, Сотрудник: Алисей Алисеич, Зарплата: 35 000,00 руб., Возраст: 64 лет,
+        ID: 5, Сотрудник: Петр, Зарплата: 55 000,00 руб., Возраст: 18 лет,
+        Средняя зарплата по больнице: 34 000,00 руб.
+        Средний возраст по больнице: 51.
+* */
